a1abc3d4e4ed45eac0bc42229cded3764e69c8a9
Ty-Waltz <ty.waltz@yahoo.com>
2024-07-19 22:20:33 -0400

    Ready to submit

--- Task2.py
+++ Task2.py
@@ -1,6 +1,8 @@
-print("how are you feeling today?")
-input() 
-if  "happy" :
+print("how are you feeling today?") 
+response = input() 
+
+if (response =="happy"):
     print("That's great to hear!")
-if "sad" :
+
+if(response =="sad"):
     print("I hope your day gets better!")
